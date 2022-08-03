c3a35fc086a9c0bbdabcce332da5eff06ed232e9
superiorkid <bayipetani@protonmail.com>
2022-08-03 22:02:10 +0800

    test usermodels

---
+++ tests/test_user_models.py
@@ -0,0 +1,15 @@
+import unittest
+from app.models import User
+
+
+class UserModelTest(unittest.TestCase):
+
+  def test_password_getter(self):
+    u = User(password = 'cat')
+    self.assertTrue(u.password_hash is not None)
+
+  def test_password_match(self):
+    u = User(password = 'cat')
+    self.assertTrue(u.check_password('cat'))
+    self.assertFalse(u.check_password('dog'))
+  
