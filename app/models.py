87bde5af8dffc2d1920e593fdf1c71682f3d0044
superiorkid <bayipetani@protonmail.com>
2022-08-03 15:21:29 +0800

    setter dan getter untuk password_hash

--- app/models.py
+++ app/models.py
@@ -1,5 +1,7 @@
-from enum import unique
+from tokenize import String
+from xmlrpc.client import Boolean
 from app import db
+from werkzeug.security import generate_password_hash, check_password_hash
 
 class User(db.Model):
   id = db.Column(db.Integer, primary_key=True)
@@ -8,3 +10,14 @@ class User(db.Model):
 
   def __repr__(self) -> str:
     return f'<User {self.username}>'
+
+  @property
+  def password(self):
+    raise AttributeError('password is not a readable attributes')
+
+  @password.setter
+  def password(self, password: str) -> None:
+    self.password_hash = generate_password_hash(password)
+  
+  def check_password(self, password: str) -> bool:
+    return check_password_hash(self.password_hash, password)
