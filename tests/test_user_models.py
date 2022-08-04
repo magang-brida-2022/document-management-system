7dcbb0926235a9073cbfb1c5437f592a8ce25fda
superiorkid <bayipetani@protonmail.com>
2022-08-04 20:33:54 +0800

    add flask form and install autopep8

--- tests/test_user_models.py
+++ tests/test_user_models.py
@@ -4,12 +4,11 @@ from app.models import User
 
 class UserModelTest(unittest.TestCase):
 
-  def test_password_getter(self):
-    u = User(password = 'cat')
-    self.assertTrue(u.password_hash is not None)
+    def test_password_setter(self):
+        u = User(password='cat')
+        self.assertTrue(u.password_hash is not None)
 
-  def test_password_match(self):
-    u = User(password = 'cat')
-    self.assertTrue(u.check_password('cat'))
-    self.assertFalse(u.check_password('dog'))
-  
+    def test_password_match(self):
+        u = User(password='cat')
+        self.assertTrue(u.check_password('cat'))
+        self.assertFalse(u.check_password('dog'))
