5870050869c505b380f81df766059ed799792280
superiorkid <bayipetani@protonmail.com>
2022-08-08 10:24:05 +0800

    update user table and create role table + migrations

--- app/models.py
+++ app/models.py
@@ -1,3 +1,4 @@
+from email.policy import default
 from werkzeug.security import generate_password_hash, check_password_hash
 from typing import Union
 from flask_login import UserMixin
@@ -9,6 +10,11 @@ class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     password_hash = db.Column(db.String(120))
+    role_id = db.Column(db.Integer, db.ForeignKey('role.id'))
+    nama_lengkap = db.Column(db.String(50))
+    jabatan = db.Column(db.String(35))
+    foto_profile = db.Column(db.String(
+        250), default="http://www.gravatar.com/avatar/3b3be63a4c2a439b013787725dfce802?d=identicon")
 
     def __repr__(self) -> str:
         return f'<User {self.username}>'
@@ -28,3 +34,14 @@ class User(UserMixin, db.Model):
 @login_manager.user_loader
 def load_user(user_id: Union[str, int]) -> int:
     return User.query.get(int(user_id))
+
+
+class Role(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), unique=True)
+    default = db.Column(db.Boolean, default=False, index=True)
+    permissions = db.Column(db.Integer)
+    users = db.relationship('User', backref="role", lazy='dynamic')
+
+    def __repr__(self) -> str:
+        return f'<Role {self.name}>'
