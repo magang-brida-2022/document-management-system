ccc6f8d46f7352efbb1357f66edb31faa4f0e81d
superiorkid <bayipetani@protonmail.com>
2022-08-15 10:43:35 +0800

    add user role

--- app/main/views.py
+++ app/main/views.py
@@ -1,5 +1,7 @@
 from flask import render_template
 from flask_login import login_required
+from app.decorators import admin_required, permission_required
+from app.models import Permission
 
 from . import main
 
@@ -13,3 +15,24 @@ def index():
 @login_required
 def protected_routes():
     return 'only authenticate users are allowed!'
+
+
+@main.get('/admin')
+@login_required
+@admin_required
+def for_admin_only():
+    return "For administrators!"
+
+
+@main.get('/pegawaitu')
+@login_required
+@permission_required(Permission.SURAT_KELUAR)
+def for_admin_tu():
+    return "for admin tu"
+
+
+@main.get('/pegawai')
+@login_required
+@permission_required(Permission.SURAT_PERMOHONAN)
+def for_pagawai():
+    return "for staff"
--- config.py
+++ config.py
@@ -5,8 +5,10 @@ from dotenv import load_dotenv
 basedir = os.path.abspath(os.path.dirname(__file__))
 load_dotenv(os.path.join(basedir, '.env'))
 
-class Config(object):
-  SECRET_KEY = os.getenv('SECRET_KEY') or os.urandom(12)
-  SQLALCHEMY_DATABASE_URI = 'postgresql://superiorkid:root@localhost/surat'
-  SQLALCHEMY_TRACK_MODIFICATIONS = False
 
+class Config(object):
+    SECRET_KEY = os.getenv('SECRET_KEY') or os.urandom(12)
+    SQLALCHEMY_DATABASE_URI = 'postgresql://superiorkid:root@localhost/surat'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    IS_ADMIN = "susan@example.com"
