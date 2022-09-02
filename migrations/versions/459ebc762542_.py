06ad8c0f82f222d5940aeecbe5a58f2b5d0e6da6
superiorkid <bayipetani@protonmail.com>
2022-09-02 08:11:37 +0800

    new migrations

---
+++ migrations/versions/459ebc762542_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 459ebc762542
+Revises: 
+Create Date: 2022-09-01 10:28:05.858932
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '459ebc762542'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
