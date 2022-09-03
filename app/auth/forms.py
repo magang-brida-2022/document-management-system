1e084e7303471401dbd44c5375e30d70ee057ef0
superiorkid <bayipetani@protonmail.com>
2022-09-03 10:02:33 +0800

    confusing change

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,9 +1,9 @@
 from xml.dom import ValidationErr
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, SelectField
 from wtforms.validators import DataRequired, Length, EqualTo
 
-from ..models import User
+from ..models import User, Bidang
 
 
 class LoginForm(FlaskForm):
@@ -24,9 +24,15 @@ class RegistrationForm(FlaskForm):
     confirm_password = PasswordField(
         "Confirm Password", validators=[DataRequired()])
     nama_lengkap = StringField('Nama Lengkap', validators=[Length(0, 64)])
+    bidang = SelectField('Bidang', coerce=int)
     jabatan = StringField('Jabatan', validators=[Length(0, 64)])
     no_telpon = StringField('No Telpon')
     submit = SubmitField('Register')
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.bidang.choices = [(bidang.id, bidang.nama_bidang)
+                               for bidang in Bidang.query.order_by(Bidang.alias).all()]
 
     def validate_username(self, username):
         if User.query.filter_by(username=username.data).first():
--- app/main/forms.py
+++ app/main/forms.py
@@ -1,7 +1,10 @@
+from xmlrpc.client import Boolean
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileField
-from wtforms import StringField, DateField,  SubmitField, TextAreaField
+from wtforms import StringField, DateField,  SubmitField, TextAreaField, SelectField, BooleanField
 from wtforms.validators import DataRequired
+
+from ..models import Bidang
 
 
 class SuratMasukForm(FlaskForm):
@@ -12,7 +15,15 @@ class SuratMasukForm(FlaskForm):
     tanggal_diterima = DateField(
         "Tanggal Diterima", validators=[DataRequired()])
     lampiran = FileField("Lampiran", validators=[DataRequired()])
+    disposisi = SelectField('Disposisi', coerce=int)
+    dilihat = BooleanField('Sudah Dilihat')
+
     submit = SubmitField('Simpan')
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.disposisi.choices = [(bidang.id, bidang.nama_bidang)
+                                  for bidang in Bidang.query.order_by(Bidang.alias).all()]
 
 
 class SuratKeluarForm(FlaskForm):
