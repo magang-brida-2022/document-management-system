282b8f472c99a3cc4fb5604aa1ff1e7b0cdddbcf
superiorkid <bayipetani@protonmail.com>
2022-09-12 20:49:37 +0800

    add :: delete & edit daily activity

--- app/daily_activity/views.py
+++ app/daily_activity/views.py
@@ -3,7 +3,7 @@ from flask_login import login_required
 from datetime import datetime
 
 from . import daily_activity
-from .forms import DailyActivityForm
+from .forms import DailyActivityForm, EditDailyActivityForm
 from ..models import DailyActivity
 from .. import db
 
@@ -24,10 +24,34 @@ def daily():
                                      deskripsi=form.deskripsi.data, output=form.output.data)
         db.session.add(new_activity)
         db.session.commit()
-        flash("Aktivitas Berhasil Ditambahkan", "Success")
+        flash("Aktivitas Berhasil Ditambahkan", "success")
         return redirect(request.url)
 
     return render_template('daily_activity/daily_activity.html', form=form, title="Daily Activity", daily_activity=daily_activity)
+
+
+@daily_activity.get('<id>/edit')
+@daily_activity.post('<id>/edit')
+@login_required
+def edit_activity(id):
+    form = EditDailyActivityForm()
+    activity = DailyActivity.query.filter_by(id=id).first()
+
+    if form.validate_on_submit():
+        activity.kegiatan = form.kegiatan.data
+        activity.tanggal = form.tanggal.data
+        activity.deskripsi = form.deskripsi.data
+        activity.output = form.output.data
+
+        db.session.commit()
+        flash("Edited Successfully", "success")
+        return redirect(url_for('daily_activity.daily'))
+
+    form.kegiatan.data = activity.kegiatan
+    form.tanggal.data = activity.tanggal
+    form.deskripsi.data = activity.deskripsi
+    form.output.data = activity.output
+    return render_template('daily_activity/edit_daily_activity.html', form=form)
 
 
 @daily_activity.get('/<id>/delete')
@@ -37,5 +61,5 @@ def delete_aktivity(id):
     del_aktivity = DailyActivity.query.get_or_404(id)
     db.session.delete(del_aktivity)
     db.session.commit()
-    flash("Aktivitas berhasil di Hapus", "Success")
+    flash("Aktivitas berhasil di Hapus", "success")
     return redirect(url_for('daily_activity.daily'))
